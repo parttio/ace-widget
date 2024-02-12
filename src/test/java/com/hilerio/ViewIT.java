d79c1865a8f99fe2365fd768a747b3ab43d87999
Sami Ekblad <sami@vaadin.com>
2024-02-12 13:55:18 +0200

    Upgrade integration tests and selenium drivers

--- src/test/java/com/hilerio/ViewIT.java
+++ src/test/java/com/hilerio/ViewIT.java
@@ -1,5 +1,6 @@
 package com.hilerio;
 
+import com.vaadin.flow.component.grid.testbench.GridElement;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,6 +10,10 @@ public class ViewIT extends AbstractViewTest {
 
     @Test
     public void componentWorks() {
+        // Select the first line on grid
+        final GridElement grid = (GridElement) $(GridElement.class).first();
+        grid.getRow(0).doubleClick();
+
         final TestBenchElement paperSlider = $("ace-widget").first();
         // Check that ace-widget contains at least one other element, which means that
         // is has been upgraded to a custom element and not just rendered as an empty
